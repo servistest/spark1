3cd3c5f420d53ea7c2085fdf9f0d4527030b947a
Aliaksandr.Kavalevich2 <Aliaksandr.Kavalevich2@thomsonreuters.com>
2018-05-18 18:53:47 +0300

    Add gitignore and example9_1

---
+++ src/main/scala/book/example9_1.scala
@@ -0,0 +1,22 @@
+package book
+
+import org.apache.spark.sql.SparkSession
+
+/**
+  * Created by alex on 3.5.18.
+  */
+object example9_1 extends App {
+
+
+  val sparkSession = SparkSession.builder()
+    .appName("Analazing logs")
+    .master("local[2]")
+    .getOrCreate()
+
+
+  val input = sparkSession.sparkContext.textFile("/home/alex/spark/projects/spark1/src/main/resources/guests.json")
+
+
+
+
+}
